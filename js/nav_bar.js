81486f62955df3c83026442fe921f23a350a060f
hugowoille <hugo.woille@gmail.com>
2023-09-15 16:03:04 +0200

    adding effect in navbar links

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -3,18 +3,18 @@ const trait1 = document.getElementById("trait1");
 const trait2 = document.getElementById("trait2");
 const trait3 = document.getElementById("trait3");
 let isHamburgerClicked = false;
-const toggleNav = document.getElementsByClassName("navlinks-container");
+const navLinksContainer = document.querySelector(".navlinks-container");
+console.log("navLinksContainer:", navLinksContainer);
 const navLinks = document.querySelectorAll(".nav-links");
-console.log("navLinks:", navLinks);
 
 hamburger.addEventListener("click", () => {
 	if (!isHamburgerClicked) {
 		trait1.classList.add("trait1-effect");
 		trait2.classList.add("trait2-effect");
 		trait3.classList.add("trait3-effect");
-		toggleNav[0].style.transform = "translateX(0)";
-		toggleNav[0].style.opacity = "1";
-
+		navLinksContainer.style.transform = "translateX(0)";
+		navLinksContainer.style.opacity = "1";
+		hamburger.setAttribute("aria-expanded", true);
 		navLinks.forEach((item) => {
 			item.style.animation = "bounce 1.5s ease";
 		});
@@ -23,9 +23,9 @@ hamburger.addEventListener("click", () => {
 		trait1.classList.remove("trait1-effect");
 		trait2.classList.remove("trait2-effect");
 		trait3.classList.remove("trait3-effect");
-		toggleNav[0].style.transform = "translateX(400px)";
-		toggleNav[0].style.opacity = "0";
-
+		navLinksContainer.style.transform = "translateX(400px)";
+		navLinksContainer.style.opacity = "0";
+		hamburger.setAttribute("aria-expanded", false);
 		navLinks.forEach((item) => {
 			item.style.animation = "none";
 		});
@@ -33,15 +33,14 @@ hamburger.addEventListener("click", () => {
 	}
 });
 
+// Hiding navBar when scrolling
+
 let lastScrollTop = 0;
 // This Varibale will store the top position
 const navbar = document.getElementById("nav-bar");
-
 window.addEventListener("scroll", function () {
 	//on every scroll this funtion will be called
-
 	const scrollTop = window.scrollY;
-	console.log("scrollTop:", scrollTop);
 	if (scrollTop > lastScrollTop) {
 		//if it will be greater than the previous
 		navbar.style.transform = "translateY(-70px)";
@@ -49,7 +48,21 @@ window.addEventListener("scroll", function () {
 	} else {
 		navbar.style.transform = "translateY(0)";
 	}
-
 	lastScrollTop = scrollTop; //New Position Stored
-	console.log("lastScrollTop:", lastScrollTop);
 });
+
+// disable Transition of Navlink container when hiting 767px
+
+/* new ResizeObserver((entries) => {
+	if (entries[0].contentRect.width <= 767) {
+		navLinksContainer.style.transition = "transform 0.4s, opacity 0.4s";
+	} else {
+		navLinksContainer.style.transition = "none";
+	}
+}).observe(document.body); */
+
+new ResizeObserver((entries) => {
+	entries[0].contentRect.width <= 767
+		? (navLinksContainer.style.transition = "transform 0.4s, opacity 0.4s")
+		: (navLinksContainer.style.transition = "none");
+}).observe(document.body);
